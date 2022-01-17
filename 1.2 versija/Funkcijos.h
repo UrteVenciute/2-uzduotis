8942c1fecbdf807736143447cee90b100fd07f38
UrteVenciute <90273563+UrteVenciute@users.noreply.github.com>
2022-01-17 21:28:51 +0200

    Add files via upload

---
+++ 1.2 versija/Funkcijos.h
@@ -0,0 +1,40 @@
+#pragma once
+//#include "Spausdinimas.h"
+#include "Studentas.h"
+#include <iostream>
+#include <iomanip> 
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>    
+#include <time.h>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <chrono>
+#include <list>
+#include <numeric>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::setw;
+using std::left;
+using std::fixed;
+using std::setprecision;
+using std::sort;
+using std::vector;
+using std::ifstream;
+using std::cerr;
+using std::getline;
+using std::ofstream;
+using std::stringstream;
+using std::to_string;
+using std::list;
+using namespace std;
+
+int uzklausakiekpaz();
+
+void darbasSuFailu(vector <int>& v1, char& atsakymas);
+
